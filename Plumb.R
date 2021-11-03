f5e019054f4c08aaf685adf11874586a4e62ceeb
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-02 18:22:30 -0700

    added index.html file for practicing JSON api fetching

--- Plumb.R
+++ Plumb.R
@@ -1,10 +1,26 @@
 library(plumber)
 
+#* @apiTitle Plumber Example
+#* @param msg The message to echo
+#* @get /echo
+
 function(msg="") {
   list(msg = paste0("message is", msg, ""))  
 }
 
+#* Plot a histogram
+#* @serializer png
+#* @get /plot
+
 function() {
   rand <- norm(100)
   hist(rand)
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+  as.numeric(a) + as.numeric(b)
 }
